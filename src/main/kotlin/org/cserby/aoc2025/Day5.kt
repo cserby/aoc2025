b8bcaf7270ae28c385d9cfb627f873bca4c88924
Marton Cserbak <mcs@penneo.com>
2025-12-05 08:49:32 +0100

    feat: Day5 part2

--- src/main/kotlin/org/cserby/aoc2025/Day5.kt
+++ src/main/kotlin/org/cserby/aoc2025/Day5.kt
@@ -1,23 +1,62 @@
 package org.cserby.aoc2025
 
+import kotlin.ranges.rangeTo
+
 object Day5 {
-    private fun parse(input: String): Pair<List<Pair<Long, Long>>, List<Long>> =
+    private fun parse(input: String): Pair<List<LongRange>, List<Long>> =
         input.split("\n").let { lines ->
             val emptyLineIndex = lines.indexOfFirst { it == "" }
             parseFreshIngredientRanges(lines.take(emptyLineIndex)) to lines.drop(emptyLineIndex + 1).map { it.toLong() }
         }
 
-    private fun parseFreshIngredientRanges(lines: List<String>): List<Pair<Long, Long>> =
+    private fun parseFreshIngredientRanges(lines: List<String>): List<LongRange> =
         lines.map { line ->
             line.split("-", limit = 2).let { parts ->
-                parts[0].toLong() to parts[1].toLong()
+                parts[0].toLong()..parts[1].toLong()
             }
         }
 
     fun part1(input: String): Int =
         parse(input).let { (freshRanges, ingredients) ->
-            ingredients.count { ingredient -> freshRanges.any { ingredient in it.first..it.second } }
+            ingredients.count { ingredient -> freshRanges.any { ingredient in it } }
         }
 
-    fun part2(input: String): Int = -1
+    fun intersect(
+        rangeA: LongRange,
+        rangeB: LongRange,
+    ): List<LongRange> =
+        if (rangeA.last < rangeB.first) { // rangeA left of rangeB
+            listOf(rangeA)
+        } else if (rangeA.first > rangeB.last) { // rangeA right of rangeB
+            listOf(rangeA)
+        } else if (rangeB.first <= rangeA.first && rangeB.last >= rangeA.last) { // rangeB overlaps rangeA
+            emptyList()
+        } else if (rangeB.first > rangeA.first && rangeB.last < rangeA.last) { // rangeB inside rangeA
+            listOf(rangeA.first until rangeB.first, rangeB.last + 1..rangeA.last)
+        } else if (rangeB.first <= rangeA.first) { // rangeB cuts into rangeA from the left
+            listOf(rangeB.last + 1..rangeA.last)
+        } else { // rangeB cuts into rangeA from the right
+            listOf(rangeA.first until rangeB.first)
+        }
+
+    tailrec fun intersectAll(
+        rangeAs: List<LongRange>,
+        rangeBs: List<LongRange>,
+    ): List<LongRange> =
+        if (rangeBs.isEmpty()) {
+            rangeAs
+        } else {
+            val rangeB = rangeBs.first()
+            val newRangeAs = rangeAs.flatMap { intersect(it, rangeB) }
+            intersectAll(newRangeAs, rangeBs.drop(1))
+        }
+
+    fun part2(input: String): Long =
+        parse(input).let { (freshRanges, _) ->
+            freshRanges
+                .flatMapIndexed { index, range ->
+                    val remaining = freshRanges.drop(index + 1)
+                    intersectAll(listOf(range), remaining)
+                }.sumOf { it.last - it.first + 1 }
+        }
 }
--- src/test/kotlin/org/cserby/aoc2025/test/Day5Test.kt
+++ src/test/kotlin/org/cserby/aoc2025/test/Day5Test.kt
@@ -17,11 +17,40 @@ class Day5Test {
 
     @Test
     fun part2example() {
-        assertEquals(-1, Day5.part2(Utils.readFile("/day5.example.txt")))
+        assertEquals(14L, Day5.part2(Utils.readFile("/day5.example.txt")))
+    }
+
+    @Test
+    fun part2intersect() {
+        val rangeA = 1000L..1020L
+
+        // rangeA left of rangeB
+        assertEquals(listOf(rangeA), Day5.intersect(rangeA, 1021L..1030L))
+
+        // rangeA right of rangeB
+        assertEquals(listOf(rangeA), Day5.intersect(rangeA, 990L..999L))
+
+        // rangeB overlaps rangeA
+        assertEquals(emptyList(), Day5.intersect(rangeA, 990L..1030L))
+        assertEquals(emptyList(), Day5.intersect(rangeA, 1000L..1020L))
+
+        // rangeB inside rangeA
+        assertEquals(listOf(1000L..1004L, 1016L..1020L), Day5.intersect(rangeA, 1005L..1015L))
+
+        // rangeB cuts into rangeA from the left
+        assertEquals(listOf(1006L..1020L), Day5.intersect(rangeA, 990L..1005L))
+        assertEquals(listOf(1006L..1020L), Day5.intersect(rangeA, 1000L..1005L))
+        assertEquals(listOf(1001L..1020L), Day5.intersect(rangeA, 990L..1000L))
+        assertEquals(listOf(1001L..1020L), Day5.intersect(rangeA, 1000L..1000L))
+
+        // rangeB cuts into rangeA from the right
+        assertEquals(listOf(1000L..1019L), Day5.intersect(rangeA, 1020L..1025L))
+        assertEquals(listOf(1000L..1019L), Day5.intersect(rangeA, 1020L..1020L))
+        assertEquals(listOf(1000L..1000L), Day5.intersect(rangeA, 1001L..1050L))
     }
 
     @Test
     fun part2() {
-        assertEquals(-1, Day5.part2(Utils.readFile("/day5.input.txt")))
+        assertEquals(359526404143208L, Day5.part2(Utils.readFile("/day5.input.txt")))
     }
 }
